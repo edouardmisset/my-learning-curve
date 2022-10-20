a85317808ddf0b5911f7a957aa3e9a2658633863
Edouard Misset <edouard.misset@intest.info>
2022-10-20 14:23:46 +0200

    Array push vs unshift performances

---
+++ Snippets/JS/array_push_unshift.js
@@ -0,0 +1,58 @@
+const arrayLength = 150_000
+const someValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// Unshift
+
+const unshifted = [...someValues]
+console.time('Unshift')
+
+while (unshifted.length < arrayLength) {
+  unshifted.unshift(NaN)
+}
+
+console.log(unshifted)
+console.timeEnd('Unshift') // 1032.787841796875 ms
+
+// Push
+
+console.time('Push and reverse')
+const pushed = [...someValues].reverse() // 38.06005859375 ms
+
+while (pushed.length < arrayLength) {
+  pushed.push(NaN)
+}
+
+pushed.reverse()
+
+console.log(pushed)
+console.timeEnd('Push and reverse')
+
+// Create new array with spread
+
+console.time('New spread array')
+
+let spreadCreated
+const someLength = someValues.length
+if (someValues.length < arrayLength) {
+  spreadCreated = [
+    ...Array(arrayLength - someValues.length).fill(NaN),
+    ...someValues,
+  ]
+}
+
+console.log(spreadCreated)
+console.timeEnd('New spread array') // 39.906982421875 ms
+
+// Create new array with concat
+
+console.time('New concat array')
+
+let concatCreated
+if (someValues.length < arrayLength) {
+  concatCreated = Array(arrayLength - someValues.length)
+    .fill(NaN)
+    .concat(someValues)
+}
+
+console.log(concatCreated)
+console.timeEnd('New concat array') // 14.547119140625 ms

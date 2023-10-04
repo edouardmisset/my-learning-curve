055b23011d35eaa59df1baeeb320c9419351e61c
Edouard <63284636+edouardmisset@users.noreply.github.com>
2023-10-04 10:55:35 +0200

    Create type-helpers.ts
    add prettify TS helper

---
+++ Snippets/TS/type-helpers.ts
@@ -0,0 +1,8 @@
+/**
+* A TypeScript type alias called `Prettify`.
+* It takes a type as its argument and returns a new type that has the same properties as the original type, 
+* but the properties are not intersected. This means that the new type is easier to read and understand.
+*/
+type Prettify<T> = {
+	[K in keyof T]: T[K];
+} & {};

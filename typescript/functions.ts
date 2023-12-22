c4893f2ef0eda8e08826c41234c9bc3dc16074e4
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2023-12-22 12:37:05 +0100

    refactor: remove function

--- typescript/functions.ts
+++ typescript/functions.ts
@@ -1,12 +1,32 @@
-const objectNullifier = <T = unknown>(maybeUndefinedObj: Partial<T>): T => {
-  const objectEntries = Object.entries(maybeUndefinedObj).map(e => [
-    e[0],
-    e[1] === undefined ? null : e[1],
-  ])
+import { objectKeys, ObjectType } from './type-helpers'
 
-  return Object.fromEntries(objectEntries)
-}
-
+/**
+ * @description Performs a shallow comparison between two objects.
+ *
+ * This function checks if the two provided objects have the same keys and if the values for these keys are strictly equal.
+ * It does not perform a deep comparison, so if the values are objects themselves, it will only check if they are the same instance, not if their content is the same.
+ * 
+ * @template Type A type that extends ObjectType. ObjectType is a type
+ * representing any object (ObjectType<T = any> = Record<string, T>). By default, Type is ObjectType.
+ *
+ * @param {Type} leftObject The first object to compare.
+ * @param {Type} rightObject The second object to compare.
+ *
+ * @returns {boolean} Returns true if the objects are **shallowly** equal, false otherwise.
+ *
+ * @example
+ * const obj1 = { a: 1, b: 2 };
+ * const obj2 = { a: 1, b: 2 };
+ * const areEqual = shallowComparison(obj1, obj2); // true
+ * 
+ * const obj1 = { a: 1, b: { c: 2 } };
+ * const obj2 = { a: 1, b: { c: 3 } };
+ * const areEqual = shallowComparison(obj1, obj2); // false
+ * 
+ * const obj1 = { a: 1, b: { c: 2 } };
+ * const obj2 = { a: 1, b: { c: 2 } };
+ * const areEqual = shallowComparison(obj1, obj2); // false
+ */
 export const shallowComparison = <Type extends ObjectType = ObjectType>(
   leftObject: Type,
   rightObject: Type,

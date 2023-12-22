7dec231f641250e5f13e939e3b4165c2d271ec46
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2023-12-22 12:48:37 +0100

    docs: 📝 document find node functions

--- typescript/find-node-in-tree.ts
+++ typescript/find-node-in-tree.ts
@@ -14,10 +14,27 @@ type SearchTreeListParameters<NodeType extends BasicNode = BasicNode> = Omit<
   nodeList: NodeType[]
 }
 /**
- * @description
- * @date 04/10/2023
- * @param {SearchTreeParameters<NodeType>} params
- * @returns {undefined | NodeType}  {(undefined | NodeType)}
+ * @description Searches a tree of nodes for a node that matches the given search term.
+ *
+ * This function takes a node and a search term, and searches the node and its sub-nodes for a node where `node[searchItemField]` equals `searchTerm`.
+ * It returns the first matching node, or `undefined` if no such node is found.
+ *
+ * This function uses the {@link searchTreeList} function to search a list of nodes.
+ * 
+ * @template NodeType A type that extends `BasicNode`. This is the type of the nodes in the tree.
+ *
+ * @param {SearchTreeParameters<NodeType>} params The parameters for the search. This is an object that contains:
+ * - `node`: The root node of the tree to search.
+ * - `searchTerm`: The term to search for.
+ * - `subItemsField`: The name of the field that contains the sub-nodes of a node.
+ * - `searchItemField`: The name of the field to compare with the search term.
+ *
+ * @returns {undefined | NodeType} The first node where `node[searchItemField]` equals `searchTerm`, or `undefined` if no such node is found.
+ *
+ * @example
+ * const node = { id: 1, name: 'Node 1', children: [{ id: 2, name: 'Node 2' }] };
+ * const params = { node, searchTerm: 'Node 2', subItemsField: 'children', searchItemField: 'name' };
+ * const foundNode = searchTree(params); // { id: 2, name: 'Node 2' }
  */
 export const searchTree = <NodeType extends BasicNode = BasicNode>(
   params: SearchTreeParameters<NodeType>,
@@ -43,14 +60,30 @@ export const searchTree = <NodeType extends BasicNode = BasicNode>(
       searchItemField,
     })
   }
-  return undefined
 }
 
 /**
- * @description
- * @date 04/10/2023
- * @param {SearchTreeListParameters<NodeType>} params
- * @returns {*}  {(undefined | NodeType)}
+ * @description Searches a list of nodes for a node that matches the given search term.
+ *
+ * This function iterates over a list of nodes and uses the `searchTree` function to search each node and its sub-nodes.
+ * It returns the first node where `node[searchItemField]` equals `searchTerm`, or `undefined` if no such node is found.
+ *
+ * This function uses the {@link searchTree} function to search each node.
+ * 
+ * @template NodeType A type that extends `BasicNode`. This is the type of the nodes in the list.
+ *
+ * @param {SearchTreeListParameters<NodeType>} params The parameters for the search. This is an object that contains:
+ * - `nodeList`: The list of nodes to search.
+ * - `searchTerm`: The term to search for.
+ * - `subItemsField`: The name of the field that contains the sub-nodes of a node.
+ * - `searchItemField`: The name of the field to compare with the search term.
+ *
+ * @returns {undefined | NodeType} The first node where `node[searchItemField]` equals `searchTerm`, or `undefined` if no such node is found.
+ *
+ * @example
+ * const nodes = [{ id: 1, name: 'Node 1', children: [{ id: 2, name: 'Node 2' }] }];
+ * const params = { nodeList: nodes, searchTerm: 'Node 2', subItemsField: 'children', searchItemField: 'name' };
+ * const foundNode = searchTreeList(params); // { id: 2, name: 'Node 2' }
  */
 export function searchTreeList<NodeType extends BasicNode = BasicNode>(
   params: SearchTreeListParameters<NodeType>,
@@ -58,9 +91,8 @@ export function searchTreeList<NodeType extends BasicNode = BasicNode>(
   const { nodeList, searchTerm, subItemsField, searchItemField } = params
 
   let foundNode: undefined | NodeType
-  // eslint-disable-next-line no-restricted-syntax
   for (const node of nodeList) {
-    foundNode ??= searchTree({
+    foundNode = searchTree({
       node,
       searchTerm,
       subItemsField,
@@ -68,5 +100,4 @@ export function searchTreeList<NodeType extends BasicNode = BasicNode>(
     })
     if (foundNode) return foundNode
   }
-  return foundNode
 }

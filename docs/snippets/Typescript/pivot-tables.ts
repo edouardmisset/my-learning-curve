984a844aef302d3621f86f1431448bb9b6c1688b
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2024-01-16 16:40:25 +0100

    wip

--- docs/snippets/Typescript/pivot-tables.ts
+++ docs/snippets/Typescript/pivot-tables.ts
@@ -8,10 +8,12 @@ type Ascent = {
   climber: string
   routeOrBoulder: string
   numberOfTries: number
+  id: number
 }
 
 const ascentList: Ascent[] = [
   {
+    id: 0,
     routeName: 'Bodybuilder',
     topoGrade: '7a',
     date: new Date('2015-08-01'),
@@ -21,6 +23,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 5,
   },
   {
+    id: 2,
     routeName: 'Le Plongeoir',
     topoGrade: '7b',
     date: new Date('2015-08-02'),
@@ -30,6 +33,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 5,
   },
   {
+    id: 1,
     routeName: 'Siegfried Gauche',
     topoGrade: '7a',
     date: new Date('2017-06-25'),
@@ -39,6 +43,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 1,
   },
   {
+    id: 3,
     routeName: 'Highway Paradise',
     topoGrade: '7b+',
     date: new Date('2017-08-04'),
@@ -48,6 +53,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 1,
   },
   {
+    id: 4,
     routeName: 'Ballade pour Vincent',
     topoGrade: '7a+',
     date: new Date('2018-03-25'),
@@ -57,6 +63,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 1,
   },
   {
+    id: 6,
     routeName: 'Le Loire',
     topoGrade: '7a',
     date: new Date('2018-06-17'),
@@ -66,6 +73,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 2,
   },
   {
+    id: 5,
     routeName: 'Cococaline',
     topoGrade: '7a+',
     date: new Date('2018-06-17'),
@@ -75,6 +83,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 1,
   },
   {
+    id: 8,
     routeName: 'Exquise Esquisse',
     topoGrade: '7b',
     date: new Date('2018-06-18'),
@@ -84,6 +93,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 1,
   },
   {
+    id: 7,
     routeName: 'Baby',
     topoGrade: '8a',
     date: new Date('2018-11-03'),
@@ -93,6 +103,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 3,
   },
   {
+    id: 9,
     routeName: 'Cannibale',
     topoGrade: '8a+',
     date: new Date('2020-10-04'),
@@ -102,6 +113,7 @@ const ascentList: Ascent[] = [
     numberOfTries: 10,
   },
   {
+    id: 10,
     routeName: "L'homme faible du moment",
     topoGrade: '8a',
     date: new Date('2019-03-23'),
@@ -113,8 +125,6 @@ const ascentList: Ascent[] = [
 ]
 
 const oneYearInMilliseconds = 365 * 24 * 60 * 60 * 1000
-
-const sample = ascentList
 
 type Operation<T = any, R = number> = (currentValue: R, row: T) => R
 
@@ -163,53 +173,29 @@ const createPivotTable = <
   )
 }
 
+// Operations
+
 // Count operation
 export const countOperation: Operation = (currentValue, _row) =>
   currentValue + 1
 
 // Sum operation
-export const sumOperation: Operation = (currentValue, row) =>
-  currentValue + row.value
-
-// Average operation
-// let averageOperation: Operation = (currentValue, row, count = 0) =>
-//   (currentValue * count + row.value) / (count + 1)
+export const sumOperationCreator =
+  (valueKey: string): Operation =>
+  (currentValue, row) =>
+    currentValue + row[valueKey]
 
 // Max operation
-export const maxOperation: Operation = (currentValue, row) =>
-  Math.max(currentValue, row.value)
+export const maxOperationCreator =
+  (valueKey: string): Operation =>
+  (currentValue, row) =>
+    Math.max(currentValue, row[valueKey])
 
 // Min operation
-export const minOperation: Operation = (currentValue, row) =>
-  currentValue === 0 ? row.value : Math.min(currentValue, row.value)
-
-type FilterDateCreator = (
-  params:
-    | { referenceDate?: Date; durationInMilliseconds?: number }
-    | { year: number }
-    | undefined,
-) => (date: Date) => boolean
-
-export const filterDateCreator: FilterDateCreator = (params = {}) => {
-  if ('year' in params) return date => date.getUTCFullYear() === params.year
-
-  const {
-    referenceDate = new Date(),
-    durationInMilliseconds = oneYearInMilliseconds,
-  } = params
-
-  return date =>
-    date.getUTCMilliseconds() >=
-    referenceDate.getUTCMilliseconds() - durationInMilliseconds
-}
-
-export const filter2024 = filterDateCreator({ year: 2024 })
-export const filter2023 = filterDateCreator({ year: 2023 })
-export const filterLast12Months = filterDateCreator({
-  durationInMilliseconds: oneYearInMilliseconds,
-})
-
-const samSample = [...sample].filter(item => filterLast12Months(item.date))
+export const minOperationCreator =
+  (valueKey: string): Operation =>
+  (currentValue, row) =>
+    currentValue === 0 ? row[valueKey] : Math.min(currentValue, row[valueKey])
 
 const pivotTable2 = createPivotTable({
   data: ascentList,
@@ -217,42 +203,174 @@ const pivotTable2 = createPivotTable({
   columnKey: 'numberOfTries',
   operation: countOperation,
 })
-console.log(pivotTable2)
+// window.console.log(pivotTable2)
 
-/*
+const pivotTableSum = createPivotTable({
+  data: ascentList,
+  rowKey: 'topoGrade',
+  columnKey: 'numberOfTries',
+  operation: sumOperationCreator('topoGrade'),
+})
+// window.console.log('🚀 ~ pivotTableSum:', pivotTableSum)
 
-const pivotTableSum = createPivotTable(sample, 'grade', 'tries', sumOperation)
-console.log('🚀 ~ pivotTableSum:', pivotTableSum)
-// let pivotTableAverage = createPivotTable(sample, 'grade', 'tries', averageOperation);
-// console.log("🚀 ~ pivotTableAverage:", pivotTableAverage)
-const pivotTableMax = createPivotTable(sample, 'grade', 'tries', maxOperation)
-console.log('🚀 ~ pivotTableMax:', pivotTableMax)
-const pivotTableMin = createPivotTable(sample, 'grade', 'tries', minOperation)
-console.log('🚀 ~ pivotTableMin:', pivotTableMin)
+const pivotTableMax = createPivotTable({
+  data: ascentList,
+  rowKey: 'topoGrade',
+  columnKey: 'numberOfTries',
+  operation: maxOperationCreator('topoGrade'),
+})
+// window.console.log('🚀 ~ pivotTableMax:', pivotTableMax)
+const pivotTableMin = createPivotTable({
+  data: ascentList,
+  rowKey: 'topoGrade',
+  columnKey: 'numberOfTries',
+  operation: minOperationCreator('topoGrade'),
+})
+// window.console.log('🚀 ~ pivotTableMin:', pivotTableMin)
 
-export const localSortStrings = (
-  left: string,
-  right: string,
-  ascending = true,
-): number => left.localeCompare(right) * (ascending ? 1 : -1)
+// Sort
 
-export const sortNumber = (
-  left: number,
-  right: number,
-  ascending = true,
-): number => {
-  if (Number.isNaN(left)) return 1
-  if (Number.isNaN(right)) return -1
-  return (left - right) * (ascending ? 1 : -1)
+export const sortStringsCreator =
+  (key?: string) =>
+  (
+    left: Record<string, unknown> | string,
+    right: Record<string, unknown> | string,
+    ascending = true,
+  ): number => {
+    // Check if left and right are strings or objects
+    const isString =
+      typeof left === 'string' && typeof right === 'string' && key !== undefined
+
+    const leftStr = isString ? left : left[key]
+    const rightStr = isString ? right : right[key]
+
+    return leftStr.localeCompare(rightStr) * (ascending ? 1 : -1)
+  }
+
+// window.console.log(
+//   '🚀 ~ sortStrings:',
+//   ascentList.sort(sortStringsCreator('routeName')),
+// )
+
+export const sortNumberCreator =
+  (key?: string) =>
+  (
+    left: Record<string, unknown> | number,
+    right: Record<string, unknown> | number,
+    ascending = true,
+  ): number => {
+    const isNumber =
+      typeof left === 'number' && typeof right === 'number' && key !== undefined
+    const leftNum = isNumber ? left : left[key]
+    const rightNum = isNumber ? right : right[key]
+
+    if (Number.isNaN(leftNum)) return 1
+    if (Number.isNaN(rightNum)) return -1
+
+    return (leftNum - rightNum) * (ascending ? 1 : -1)
+  }
+
+// window.console.log(
+//   '🚀 ~ sortNumber:',
+//   ascentList.sort(sortNumberCreator('numberOfTries')),
+// )
+
+export const sortDateCreator = (key?: string) => {
+  return (
+    left: Record<string, unknown> | Date,
+    right: Record<string, unknown> | Date,
+    ascending = true,
+  ): number => {
+    const isDate =
+      left instanceof Date && right instanceof Date && key !== undefined
+    const leftDate = isDate ? left : left[key]
+    const rightDate = isDate ? right : right[key]
+
+    if (Number.isNaN(leftDate.getTime())) return 1
+    if (Number.isNaN(rightDate.getTime())) return -1
+    return (leftDate.getTime() - rightDate.getTime()) * (ascending ? 1 : -1)
+  }
 }
 
-export const sortDate = (left: Date, right: Date, ascending = true): number => {
-  if (Number.isNaN(left.getTime())) return 1
-  if (Number.isNaN(right.getTime())) return -1
-  return (left.getTime() - right.getTime()) * (ascending ? 1 : -1)
+// window.console.log('🚀 ~ sortDate:', ascentList.sort(sortDateCreator('date')))
+
+// Filter
+
+// Date
+type FilterDateCreator = (
+  params: // TODO add another type of param where we filter between two dates
+  | { startDate: Date; endDate: Date }
+    | { referenceDate?: Date; durationInMilliseconds?: number }
+    | { year: number }
+    | undefined,
+) => (date: Date) => boolean
+
+export const filterDateCreator: FilterDateCreator = (params = {}) => {
+  if ('year' in params) return date => date.getFullYear() === params.year
+
+  if ('startDate' in params && 'endDate' in params)
+    return date =>
+      date.getTime() >= params.startDate.getTime() &&
+      date.getTime() <= params.endDate.getTime()
+
+  const {
+    referenceDate = new Date(),
+    durationInMilliseconds = oneYearInMilliseconds,
+  } = params
+
+  return date =>
+    date.getTime() >= referenceDate.getTime() - durationInMilliseconds
 }
 
+export const filter2018 = filterDateCreator({ year: 2018 })
+export const filter2023 = filterDateCreator({ year: 2023 })
+export const filterLast12Months = filterDateCreator({
+  durationInMilliseconds: oneYearInMilliseconds,
+})
+export const filterLast5years = filterDateCreator({
+  durationInMilliseconds: 5 * oneYearInMilliseconds,
+})
 
+// window.console.log(
+//   '🚀 ~ filter2024:',
+//   ascentList.filter(({ date }) => filter2018(date)),
+// )
+// window.console.log(
+//   '🚀 ~ filter5Years:',
+//   ascentList.filter(({ date }) => filterLast5years(date)),
+// )
+
+// Boolean
+
+export const filterBooleanCreator =
+  (key?: string) =>
+  (value: boolean | Record<string, unknown>): boolean => {
+    if (typeof value === 'boolean') return value
+
+    if (typeof value?.[key] === 'boolean') return value[key] === true
+
+    throw new Error(`Invalid key (${key}) or value (${value})`)
+  }
+
+// Find
+
+export const findCreator =
+  <Obj extends Record<string, unknown>>(key: keyof Obj) =>
+  (value: unknown) =>
+  (obj: Obj): boolean => {
+    if (!Object.prototype.hasOwnProperty.call(obj, key))
+      throw new Error(`Key "${String(key)}" does not exist in the object.`)
+
+    return obj[key] === value
+  }
+
+export const findById = findCreator('id')
+export const findByRouteName = findCreator('routeName')
+
+// window.console.log(ascentList.find(findById(1)))
+// window.console.log(ascentList.find(findByRouteName('Bodybuilder')))
+
+// Transpose pivot table
 
 // TODO WIP
 export function transposePivotTable(table: {
@@ -274,5 +392,3 @@ export function transposePivotTable(table: {
   }
   return transposed
 }
-
-*/

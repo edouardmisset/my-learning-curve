a1d28998bfa7b91e43b586c1d675b5cd5e4f52b0
Edouard Misset <edouardmisset@gmail.com>
2024-01-27 00:26:55 +0100

    fix: fix isObject function

--- docs/snippets/Typescript/Object/is-object.ts
+++ docs/snippets/Typescript/Object/is-object.ts
@@ -1,19 +1,15 @@
 /**
- * Checks if a given value is a plain object.
- *
- * A plain object in JavaScript is an object that is created by the `Object` constructor or one that has `null` as its prototype.
+ * Checks if a given value is an object.
  *
  * @param {unknown} val - The value to check.
- * @returns {val is Record<string, unknown>} - Returns `true` if the value is a plain object, else `false`.
+ * @returns {val is Record<string, unknown>} - Returns `true` if the value is an object, else `false`.
  *
  * @example
- * isPlainObject({}) // returns true
- * isPlainObject({ a: 1 }) // returns true
- * isPlainObject([]) // returns false
- * isPlainObject(null) // returns false
- * isPlainObject('Hello, world!') // returns false
+ * isObject({}) // returns true
+ * isObject({ a: 1 }) // returns true
+ * isObject([]) // returns true
+ * isObject('Hello, world!') // returns false
  */
-export const isPlainObject = (val: unknown): val is Record<string, unknown> =>
+export const isObject = (val: unknown): val is Record<string, unknown> =>
   !!val &&
-  typeof val === 'object' &&
-  Object.getPrototypeOf(val) === Object.prototype
+  typeof val === 'object'

56fce3c56a706e1abdda3c79ab3593ed81f30f1f
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2024-01-30 10:28:46 +0100

    docs: rework isValid doc

--- docs/snippets/Typescript/Number/is-valid.ts
+++ docs/snippets/Typescript/Number/is-valid.ts
@@ -1,7 +1,13 @@
 /**
  * Checks if a value is a valid number.
+ * A valid number is a number that is not `NaN` or `Infinity`.
  * @param num - The value to check.
  * @returns A boolean indicating if the value is a valid number.
+ * @example
+ * isValidNumber(1) // true
+ * isValidNumber('1') // false
+ * isValidNumber(Infinity) // false
+ * isValidNumber(NaN) // false
  */
 export const isValidNumber = (num: unknown): num is number =>
   typeof num === 'number' && Number.isFinite(num)
@@ -11,6 +17,12 @@ export const isValidNumber = (num: unknown): num is number =>
  * @param maybeNumber - The value to convert.
  * @param fallbackValue - The fallback value to return if the conversion fails.
  * @returns The converted value if it is a valid number, otherwise the fallback value.
+ * @example
+ * validNumberWithFallback(1, 5) // 1
+ * validNumberWithFallback('1', 5) // 1
+ * validNumberWithFallback(Infinity, 5) // 5
+ * validNumberWithFallback(NaN, 5) // 5
+ * validNumberWithFallback('foo', 5) // 5
  */
 export const validNumberWithFallback = <T = number>(
   maybeNumber: unknown,

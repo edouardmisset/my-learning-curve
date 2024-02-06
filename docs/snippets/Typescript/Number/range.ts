1e15a28b36a9954782ad256e0b5dde64f937cba7
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2024-02-06 10:01:25 +0100

    refactor: refactor range functions to support inclusive/exclusive ranges

--- docs/snippets/Typescript/Number/range.ts
+++ docs/snippets/Typescript/Number/range.ts
@@ -1,12 +1,12 @@
-
 type ValueAndRange = {
   value: number
   minimum: number
   maximum: number
+  inclusive?: boolean
 }
 
 /**
- * Clamps the provided value within the specified range (inclusive).
+ * Clamps the provided value within the specified range.
  * @param {ValueAndRange} options - An object containing the maximum, minimum, and value to be clamped.
  * @param {number} options.maximum - The maximum value of the range.
  * @param {number} options.minimum - The minimum value of the range.
@@ -17,7 +17,8 @@ export const clampValueInRange = ({
   maximum,
   minimum,
   value,
-}: ValueAndRange): number => Math.max(Math.min(value, maximum), minimum)
+}: Omit<ValueAndRange, 'inclusive'>): number =>
+  Math.max(Math.min(value, maximum), minimum)
 
 /**
  * Checks if the provided value is strictly outside the specified limits.
@@ -31,4 +32,56 @@ export const isOutsideRange = ({
   maximum,
   minimum,
   value,
-}: ValueAndRange): boolean => value < minimum || maximum < value
+}: Omit<ValueAndRange, 'inclusive'>): boolean =>
+  value < minimum || maximum < value
+
+/**
+ * Checks if the provided value is within the specified range (including the min
+ * and the max).
+ * @param {ValueAndRange} options - An object containing the maximum, minimum, and value to be checked.
+ * @param {number} options.maximum - The maximum value of the range.
+ * @param {number} options.minimum - The minimum value of the range.
+ * @param {number} options.value - The value to be checked within the range.
+ * @returns {boolean} - A boolean value indicating whether the provided value is within the specified range.
+ * @see isInRange
+ */
+export const isInclusiveInRange = ({
+  maximum,
+  minimum,
+  value,
+}: ValueAndRange): boolean => minimum <= value && value <= maximum
+
+/**
+ * Checks if the provided value is within the specified range (excluding the min
+ * and the max).
+ * @param {ValueAndRange} options - An object containing the maximum, minimum, and value to be checked.
+ * @param {number} options.maximum - The maximum value of the range.
+ * @param {number} options.minimum - The minimum value of the range.
+ * @param {number} options.value - The value to be checked against the range.
+ * @returns {boolean} - A boolean value indicating whether the provided value is within the specified range.
+ */
+export const isExclusiveInRange = ({
+  maximum,
+  minimum,
+  value,
+}: ValueAndRange): boolean => minimum < value && value < maximum
+
+/**
+ * Checks if the provided value is within the specified range.
+ * @default inclusive - true
+ * @param {IsInRangeOptions} options - An object containing the maximum, minimum, and value to be checked.
+ * @param {number} options.maximum - The maximum value of the range.
+ * @param {number} options.minimum - The minimum value of the range.
+ * @param {number} options.value - The value to be checked within the range.
+ * @param {boolean} [options.inclusive=true] - A boolean value indicating whether the range is inclusive.
+ * @returns {boolean} - A boolean value indicating whether the provided value is within the specified range.
+ */
+export const isInRange = ({
+  maximum,
+  minimum,
+  value,
+  inclusive = true,
+}: ValueAndRange): boolean =>
+  inclusive
+    ? isInclusiveInRange({ maximum, minimum, value })
+    : isExclusiveInRange({ maximum, minimum, value })

0f149c5d804cbbc3dd94ec08f588b0ca74e47990
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2024-02-09 11:06:47 +0100

    feat: add frequency function

--- docs/snippets/Typescript/Array/count-by.ts
+++ docs/snippets/Typescript/Array/count-by.ts
@@ -17,3 +17,26 @@
  */
 export const countBy = <T>(arr: T[], checkFunction: (arg: T) => boolean) =>
   arr.reduce((count, value) => (checkFunction(value) ? count + 1 : count), 0)
+
+/**
+ * Calculates the frequency of each unique element in an array.
+ * @template T - The type of elements in the array, which extends string or number.
+ * @param {T[]} arr - An array of elements of type T.
+ * @returns {Record<T, number>} - An object where the keys are the unique elements from the input array and the values are their corresponding frequencies.
+ *
+ * @example
+ * frequency(['apple', 'banana', 'apple', 'cherry'])
+ * // returns { 'apple': 2, 'banana': 1, 'cherry': 1 }
+ *
+ * @example
+ * frequency([1, 2, 1, 3])
+ * // returns { '1': 2, '2': 1, '3': 1 }
+ */
+export const frequency = <const T extends string | number>(arr: T[]) =>
+  arr.reduce(
+    (acc, val) => {
+      acc[val] = (acc[val] ?? 0) + 1
+      return acc
+    },
+    {} as Record<T, number>,
+  )

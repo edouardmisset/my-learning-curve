b53fb18ffe7a4ac7d9ba8554ac65defddb0e3e57
Edouard Misset <edouardmisset@gmail.com>
2024-02-11 00:21:41 +0100

    feat: add range function to create an array of numbers

--- docs/snippets/Typescript/Array/create.ts
+++ docs/snippets/Typescript/Array/create.ts
@@ -26,3 +26,38 @@ export const createArray = <T = number>(
   // @ts-ignore
   transform: Parameters<typeof Array.from<T, U>>[1] = (_, index) => index,
 ): T[] => Array.from({ length }, transform)
+
+/**
+ * Creates an array of numbers (positive and/or negative) progressing from
+ * `start` up to `end` (included).
+ *
+ * @default step = 1
+ *
+ * @param {number} start - The start of the range.
+ * @param {number} [end] - The end of the range.
+ * @param {number} [step=1] - The value to increment or decrement by.
+ * @returns {number[]} Returns the range of numbers.
+ *
+ * @example
+ * range(4);
+ * // => [0, 1, 2, 3, 4]
+ *
+ * @example
+ * range(-4);
+ * // => [0, -1, -2, -3, -4]
+ *
+ * @example
+ * range(1, 5);
+ * // => [1, 2, 3, 4, 5]
+ *
+ * @example
+ * range(0, 20, 5);
+ * // => [0, 5, 10, 15, 20]
+ */
+export const range = (start: number, end?: number, step = 1): number[] => {
+  const adjustedStart = end === undefined ? 0 : start
+  const adjustedEnd = end === undefined ? start : end
+
+  const length = Math.ceil((adjustedEnd - adjustedStart) / step) + 1
+  return Array.from({ length }, (_, i) => adjustedStart + i * step)
+}

35d2efc29b434631e3245bb6129ef3da3720f121
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2024-02-14 17:30:04 +0100

    refactor: update sortKeys's jsdoc description

--- docs/snippets/Typescript/Object/sort-keys.ts
+++ docs/snippets/Typescript/Object/sort-keys.ts
@@ -4,14 +4,19 @@
  *
  * It assumes the keys are strings.
  *
+ *
  * @template Obj - The type of the object.
  * @param {Obj} obj - The object whose keys are to be sorted.
+ * @param {boolean} [ascending=true] - Whether to sort the keys in ascending
+ * order. If false, the keys are sorted in descending order.
  * @returns {Obj} A new object with the sorted keys.
  *
  * @example
  * const obj = { b: 1, a: 2, c: 3 };
- * const sortedObj = sortKeys(obj);
- * console.log(sortedObj); // { a: 2, b: 1, c: 3 }
+ * const sortedObjAsc = sortKeys(obj);
+ * console.log(sortedObjAsc); // { a: 2, b: 1, c: 3 }
+ * const sortedObjDesc = sortKeys(obj, false);
+ * console.log(sortedObjDesc); // { c: 3, b: 1, a: 2 }
  */
 export const sortKeys = <Obj extends Record<string, unknown>>(
   obj: Obj,

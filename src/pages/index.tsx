71f759af7017661febc40e623803af81d2e3874e
Edouard Misset <edouardmisset@gmail.com>
2025-01-21 15:17:18 +0100

    refactor: inline HomepageHeader component into Home for improved readability

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,21 @@ import Heading from '@theme/Heading'
 import Layout from '@theme/Layout'
 import clsx from 'clsx'
 
+import type React from 'react'
 import styles from './index.module.css'
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <header className={clsx('hero hero--primary', styles['hero-banner'])}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {'</>'}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  )
-}
-
-export default function Home(): JSX.Element {
+export default function Home(): React.JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <HomepageHeader />
+      <header className={clsx('hero hero--primary', styles['hero-banner'])}>
+        <div className="container">
+          <Heading as="h1" className="hero__title">
+            {'</>'}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+      </header>
       <main />
     </Layout>
   )

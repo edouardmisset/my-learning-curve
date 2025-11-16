2560f369860d6e76ee5c5ad0635e7743499fc0f3
Edouard Misset <edouardmisset@gmail.com>
2025-11-16 10:22:09 +0100

    chore: remove en directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,7 +58,7 @@ export default defineConfig({
         },
         {
           label: 'Blog',
-          autogenerate: { directory: 'blog/en' },
+          autogenerate: { directory: 'blog' },
         },
         {
           label: 'Guides',

9e00fcf4436562ffeecf5be3dbe47a19a5bc074b
Edouard Misset <edouardmisset@gmail.com>
2026-01-16 17:45:08 +0100

    refactor: move the dashboard to a separate app

--- src/components/feature-list/types.ts
+++
@@ -1,8 +0,0 @@
-export interface FeatureData {
-  id: string
-  name: string
-  description: string
-  category: 'HTML' | 'CSS' | 'JavaScript' | 'Web Platform'
-  status: 'limited' | 'newly' | 'widely' | 'unknown'
-  date: string
-}

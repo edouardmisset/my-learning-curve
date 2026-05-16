39738ebd79b3a131a3bd1434c01d0bc2f6d93807
Edouard Misset <edouardmisset@gmail.com>
2026-05-16 20:28:56 +0200

    fix: update favicon and app icons for web manifest

--- src/config/astro/starlight-options.ts
+++ src/config/astro/starlight-options.ts
@@ -15,6 +15,41 @@ export const STARLIGHT_OPTIONS = {
   description: 'Bits and pieces I learned during my coding journey!',
   tagline: 'Bits and pieces I learned during my coding journey!',
   favicon: '/favicon.ico',
+  head: [
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    },
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+    },
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    },
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+    },
+  ],
   plugins: [
     catppuccin(CATPPUCCIN_OPTIONS),
     starlightKbd(STARLIGHT_KBD_OPTIONS),
